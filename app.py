fcea0c34bd6d303136a122434fc74c1f35118d5c
John Feng <johnfengphd@gmail.com>
2025-06-15 13:23:34 -0700

    Add new document processing features and enhance navigation
    - Updated `app.py` to include new pages for the Agentic Document Parser, Pinecone Data Upload, and PDF Splitter, improving document management capabilities.
    - Introduced `agentic_doc_app.py` for parsing and visualizing documents, with options to save results in both pickle and JSON formats.
    - Added `pdf_split_merge.py` for splitting and merging PDF files, enhancing user functionality for document manipulation.
    - Created `pinecone_rag.py` for integrating retrieval-augmented generation with Pinecone, allowing users to query and visualize document chunks.
    - Improved the overall user interface and navigation structure for better accessibility to new features.

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 import streamlit as st
 
-page_1 = st.Page("streamlit_pages/page_1.py", title="Document Parser")
-page_2 = st.Page("streamlit_pages/page_2.py", title="App Storage Explorer")
-page_3 = st.Page("streamlit_pages/page_3.py", title="PDF Splitter")
-page_4 = st.Page("streamlit_pages/page_4.py", title="Document Search")
-page_5 = st.Page("streamlit_pages/pinecone_playground.py",
-                 title="Pinecone Playground")
-page_6 = st.Page("streamlit_pages/pinecone_upload.py",
-                 title="PDF to Pinecone Upload")
+agentic_doc_app = st.Page("streamlit_pages/agentic_doc_app.py", title="Agentic Doc Parser")
+# page_2 = st.Page("streamlit_pages/page_2.py", title="App Storage Explorer")
+# page_4 = st.Page("streamlit_pages/page_4.py", title="Document Search")
+pinecone_upload = st.Page("streamlit_pages/pinecone_upload.py",
+                 title="Pinecone Data Upload")
+pinecone_rag = st.Page("streamlit_pages/pinecone_rag.py",
+                 title="Pinecone RAG")
+pdf_split_merge = st.Page("streamlit_pages/pdf_split_merge.py", title="PDF Splitter")
 
-pg = st.navigation([page_1, page_2, page_3, page_4, page_5, page_6])
-st.set_page_config(page_title="Data manager",
+
+pg = st.navigation([
+    agentic_doc_app, 
+    pinecone_upload,
+    pinecone_rag, 
+    pdf_split_merge,
+                    ])
+
+st.set_page_config(page_title="Agentic Doc Parser with Pinecone RAG",
                    page_icon=":material/edit:", layout="wide")
 pg.run()
